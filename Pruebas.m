96fdd970cb4ee4ca13f94046a518eef62b992a1a
Javier Madrid <62428205+Chavivi11@users.noreply.github.com>
2022-09-16 11:23:17 +0200

    Calculated the number of classes

--- Pruebas.m
+++ Pruebas.m
@@ -1,2 +1,31 @@
+clc; close all; clear
+
 muestra = importdata("UAH_BCI_database_description\A01_1.mat");
 
+numClases = [];
+
+for i = 1:length(muestra.clase)
+    if(not(ismember(muestra.clase(i),numClases)))
+        numClases = [numClases muestra.clase(i)];
+    end
+end
+
+numClasses = numel(numClases);
+net = alexnet;
+layersTransfer = net.Layers(1:end-3);
+
+layers = [
+    layersTransfer
+    fullyConnectedLayer(numClasses,'WeightLearnRateFactor',20,'BiasLearnRateFactor',20)
+    softmaxLayer
+    classificationLayer];
+
+options = trainingOptions('sgdm', ...
+    'MiniBatchSize',10, ...
+    'MaxEpochs',6, ...
+    'InitialLearnRate',1e-4, ...
+    'Shuffle','every-epoch', ...
+    'ValidationData',augimdsValidation, ...
+    'ValidationFrequency',3, ...
+    'Verbose',false, ...
+    'Plots','training-progress');
