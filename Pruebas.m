5b0ecf3cb36f6cb805e2df26ad9758b2a1d89fc4
Javier Madrid <62428205+Chavivi11@users.noreply.github.com>
2022-09-18 12:28:52 +0200

    Net trainig

--- Pruebas.m
+++ Pruebas.m
@@ -3,6 +3,7 @@ clc; close all; clear
 muestra = importdata("UAH_BCI_database_description\A01_1.mat");
 
 numClases = [];
+trainingData = [];
 
 for i = 1:length(muestra.clase)
     if(not(ismember(muestra.clase(i),numClases)))
@@ -10,19 +11,45 @@ for i = 1:length(muestra.clase)
     end
 end
 
-Freq = (real(fft(muestra.canal) / 1e-3))
+Freq = (real(fft(muestra.canal) / 1e-3));
 
-trainingData = find(Freq>12 & Freq<33)
+for j = 1:length(Freq)
+    if(Freq(j)>12 & Freq(j)<33)
+        trainingData = [trainingData Freq(j)];
+    end
+end
 
-numClasses = numel(numClases);
+
+numDiffClasses = numel(numClases);
+
 net = alexnet;
-layersTransfer = net.Layers(1:end-3);
 
 layers = [
-    layersTransfer
-    fullyConnectedLayer(numClasses,'WeightLearnRateFactor',20,'BiasLearnRateFactor',20)
-    softmaxLayer
-    classificationLayer];
+    featureInputLayer(40,"Name","featureinput")
+    convolution1dLayer(3,32,"Name","conv1d_5","Padding","same")
+    reluLayer("Name","relu1")
+    crossChannelNormalizationLayer(5,"Name","norm1","K",1)
+    maxPooling1dLayer(5,"Name","maxpool1d_3","Padding","same")
+    convolution1dLayer(3,32,"Name","conv1d_1","Padding","same")
+    reluLayer("Name","relu2")
+    crossChannelNormalizationLayer(5,"Name","norm2","K",1)
+    maxPooling1dLayer(5,"Name","maxpool1d_1","Padding","same")
+    convolution1dLayer(3,32,"Name","conv1d_2","Padding","same")
+    reluLayer("Name","relu3")
+    convolution1dLayer(3,32,"Name","conv1d_3","Padding","same")
+    reluLayer("Name","relu4")
+    convolution1dLayer(3,32,"Name","conv1d_4","Padding","same")
+    reluLayer("Name","relu5")
+    maxPooling1dLayer(5,"Name","maxpool1d_2","Padding","same")
+    fullyConnectedLayer(4096,"Name","fc6","BiasLearnRateFactor",2)
+    reluLayer("Name","relu6")
+    dropoutLayer(0.5,"Name","drop6")
+    fullyConnectedLayer(4096,"Name","fc7","BiasLearnRateFactor",2)
+    reluLayer("Name","relu7")
+    dropoutLayer(0.5,"Name","drop7")
+    fullyConnectedLayer(1000,"Name","fc8","BiasLearnRateFactor",2)
+    softmaxLayer("Name","prob")
+    classificationLayer("Name","output")];
 
 options = trainingOptions('sgdm', ...
     'MiniBatchSize',10, ...
@@ -33,4 +60,4 @@ options = trainingOptions('sgdm', ...
     'Verbose',false, ...
     'Plots','training-progress');
 
-netTransfer = trainNetwork(muestra.muestra,layers,options);
+netTransfer = trainNetwork(trainingData,layers,options);
