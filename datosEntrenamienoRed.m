96d13a0f9d798fb9e9eaee34132e72de7c801f1e
Javier Madrid <62428205+Chavivi11@users.noreply.github.com>
2022-12-28 21:59:50 +0100

    Exportar datos

--- datosEntrenamienoRed.m
+++ datosEntrenamienoRed.m
@@ -1,10 +1,10 @@
 function [datosEntrenamiento] = datosEntrenamienoRed(manoDerecha, manoIzquierda, pies)
 
-deDosEnDos = [manoDerecha(:,1:2:28) manoIzquierda(:,1:2:28) pies(:,1:2:24)];
-deTresEnTres = [manoDerecha(:,1:3:27) manoIzquierda(:,1:3:27) pies(:,1:3:21)];
-deCuatroEnCuatro = [manoDerecha(:,1:4:28) manoIzquierda(:,1:4:28) pies(:,1:4:24)];
+deDosEnDos = [manoDerecha(:,1:2:28) manoIzquierda(:,1:2:28) pies(:,1:2:28)];
+deTresEnTres = [manoDerecha(:,1:3:27) manoIzquierda(:,1:3:27) pies(:,1:3:27)];
+deCuatroEnCuatro = [manoDerecha(:,1:4:28) manoIzquierda(:,1:4:28) pies(:,1:4:28)];
 deCincoEnCinco = [manoDerecha(:,1:5:25) manoIzquierda(:,1:5:25) pies(:,1:5:25)];
-variosDatos = [manoDerecha(:,1:28) manoIzquierda(:,1:28) pies(:,1:26)];
+variosDatos = [manoDerecha(:,1:28) manoIzquierda(:,1:28) pies(:,1:28)];
 
 datosEntrenamiento = [deDosEnDos deTresEnTres deCuatroEnCuatro deCincoEnCinco variosDatos];
 
